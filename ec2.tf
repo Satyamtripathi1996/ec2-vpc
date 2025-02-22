47eceb3230270a1f244b4ab0295bc8da3b92080d
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-02-22 21:52:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_server" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0885b1f6bd170450c"
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
