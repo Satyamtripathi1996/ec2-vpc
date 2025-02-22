eff16ebf237d6c010d37973e889aca4504151285
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-02-22 21:15:28 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "MainIGW"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
